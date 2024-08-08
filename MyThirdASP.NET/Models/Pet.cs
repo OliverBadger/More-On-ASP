08c92addfb0d55bced60f38c083b9165f2a3b175
Oliver <olibadgerwork@gmail.com>
2024-08-08 14:03:31 +0100

    Add project files.

---
+++ MyThirdASP.NET/Models/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyThirdASP.NET.Models
+{
+    public class Pet
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; }
+        public string Colour{ get; set; }
+        public DateTime DOB { get; set; }
+        public string Sound { get; set; }
+        public string SoundType { get; set; }
+    }
+}
